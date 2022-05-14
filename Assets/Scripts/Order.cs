446959128aa98b629423d172788d74837be292a6
Kaio <kaiotrinade@gmail.com>
2022-05-14 15:59:26 -0300

    Pedidos (em progresso)

---
+++ Assets/Scripts/Order.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Order : MonoBehaviour
+{
+    public Meal myMeal;
+
+    public Image mealImage;
+    public Image[] recipeIngredientsIMG;
+
+    public TextMeshProUGUI socreTMP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitializeOrder(Meal meal)
+    {
+        myMeal = meal;
+        mealImage.sprite = myMeal.image;
+
+        for (int i = 0; i < meal.recipeIngredients.Length; i++)
+        {
+            recipeIngredientsIMG[i].sprite = meal.recipeIngredients[i].;
+        }
+
+    }
+}
---
+++ Assets/Scripts/OrderManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderManager : MonoBehaviour
+{
+    public Meal[] meals;
+
+    public int[] difficultyBreakdown;
+
+    public int difficultyIndex;
+
+    public static float timeTracker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeTracker += Time.deltaTime;
+
+        if (timeTracker < difficultyBreakdown[difficultyIndex])
+        {
+            difficultyIndex++;
+        }
+
+        if (difficultyIndex == 0)
+        {
+
+        }
+    }
+
+
+    public void SpawnOrder(int difficulty)
+    {
+        List<Meal> mealsAvailable = new List<Meal>();
+
+        for (int i = 0; i < difficulty; i++)
+        {
+            if (i == 0)
+            {
+                foreach (var item in meals)
+                {
+                    if (item.difficulty == Meal.Difficulty.easy)
+                    {
+                        mealsAvailable.Add(item);
+                    }
+                }
+            }
+            else if (i == 1)
+            {
+                foreach (var item in meals)
+                {
+                    if (item.difficulty == Meal.Difficulty.normal)
+                    {
+                        mealsAvailable.Add(item);
+                    }
+                }
+            }
+            else if (i == 2)
+            {
+                foreach (var item in meals)
+                {
+                    if (item.difficulty == Meal.Difficulty.hard)
+                    {
+                        mealsAvailable.Add(item);
+                    }
+                }
+            }
+        }
+
+        //Spawnar order
+
+        int randMeal = Random.Range(0, mealsAvailable.Count);
+
+
+    }
+}
