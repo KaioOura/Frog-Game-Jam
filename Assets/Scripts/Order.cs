7313e15f620b65f2eeb18b0ee0155c9fcb3a3847
Priatel <79919583+Priatel@users.noreply.github.com>
2022-05-14 16:04:18 -0300

    Fix BT

--- Assets/Scripts/Order.cs
+++ Assets/Scripts/Order.cs
@@ -32,7 +32,7 @@ public class Order : MonoBehaviour
 
         for (int i = 0; i < meal.recipeIngredients.Length; i++)
         {
-            recipeIngredientsIMG[i].sprite = meal.recipeIngredients[i].;
+            //recipeIngredientsIMG[i].sprite = meal.recipeIngredients[i];
         }
 
     }
