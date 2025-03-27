e81f032fd9eaa5ef5168173af6723aea68063827
Tozze <gtozze@gmail.com>
2025-03-27 12:41:27 -0300

    Att de otimização

---
+++ Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSDisplay : MonoBehaviour
+{
+    private float fps;
+    public TMPro.TextMeshProUGUI FPSCounterText;
+
+    void Start()
+    {
+        InvokeRepeating("GetFPS", 1, 1);
+    }
+
+    void GetFPS()
+    {
+        fps = (int)(1f / Time.unscaledDeltaTime);
+        FPSCounterText.text = "FPS: " + fps.ToString();
+    }
+}
